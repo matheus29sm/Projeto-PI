12fdc3efb8831440ea3c5cfc1591d00b17052a24
MarceloStos <marcelostos.dev@gmail.com>
2023-05-01 21:37:12 -0300

    Começando o projeto

---
+++ Projeto Malária/App.py
@@ -0,0 +1,9 @@
+import cv2
+import sys
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+
+imagem = io.imread("0ac747cd-ff32-49bf-bc1a-3e9b7702ce9c.png")
+plt.imshow(imagem, cmap='gray')
+ 
