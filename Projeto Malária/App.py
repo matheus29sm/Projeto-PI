ceccfb33875beb17c918d36a85f6633dacd17b55
MarceloStos <marcelostos.dev@gmail.com>
2023-05-03 17:38:25 -0300

    Aperfeiçoando os raios

--- Projeto Malária/App.py
+++ Projeto Malária/App.py
@@ -12,10 +12,11 @@ import numpy as np
 import cv2
 import sys
 
+#Descomente o diretorio que funciona com voce
 
-# imagem = cv2.imread(r'C:\Users\macel\Downloads\malaria\0ac747cd-ff32-49bf-bc1a-3e9b7702ce9c.png')
+#imagem = cv2.imread(r'C:\Users\macel\Downloads\malaria\0ac747cd-ff32-49bf-bc1a-3e9b7702ce9c.png')
 # imagem = io.imread (r'C:\Users\mathe\Projeto-PI\Projeto Malária\malaria\images\0ac747cd-ff32-49bf-bc1a-3e9b7702ce9c.png') // esse funciona
-imagem = io.imread('./malaria/images/0ac747cd-ff32-49bf-bc1a-3e9b7702ce9c.png')
+#imagem = io.imread('./malaria/images/0ac747cd-ff32-49bf-bc1a-3e9b7702ce9c.png')
 canais = cv2.split(imagem)
 
 
@@ -33,7 +34,7 @@ binary = binary_closing (binaria)
 
 edges = sobel(binary)
 
-raios = np.arange(30, 45, 2)
+raios = np.arange(40, 50, 2)
 hough_grade = hough_circle (edges, raios)
 
 acumulador, a, b, raio = hough_circle_peaks (hough_grade, raios,total_num_peaks = 200)
@@ -43,7 +44,7 @@ fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(20, 8))
 image = color.gray2rgb(red_img)
 
 centers = []  # armazena os centros dos círculos já desenhados
-min_dist = 20  # distância mínima desejada entre os centros dos círculos
+min_dist = 30  # distância mínima desejada entre os centros dos círculos
 
 for center_y, center_x, radius in zip(b, a, raio):
     # verifica se o centro atual está próximo demais de um círculo já desenhado
